bf5b3b3621ec6b5e53b0dfad22ad9431a258967e
NastiaKed <113835154+NastiaKed@users.noreply.github.com>
2023-05-14 23:55:12 +0300

    Update WritingDesk.py

--- WritingDesk.py
+++ WritingDesk.py
@@ -1,9 +1,4 @@
 class WritingDesk:
-    __number_of_drawers = None
-    __has_keyboard_tray = None
-    __max_weight_capacity = None
-    __current_height = None
-    __max_height = None
 
     def __init__(self, number_of_drawers, has_keyboard_tray, max_weight_capacity, current_height, max_height):
         self.__number_of_drawers = number_of_drawers
@@ -11,7 +6,6 @@ class WritingDesk:
         self.__max_weight_capacity = max_weight_capacity
         self.__current_height = current_height
         self.__max_height = max_height
-        self.get_data()
 
     def adjust_height(self, centimeters: int):
         if self.__current_height < self.__max_height:
@@ -28,16 +22,19 @@ class WritingDesk:
         self.__current_height = current_height
         self.__max_height = max_height
 
-    def get_data(self):
-        print("Writing Desk:", " number of drawers:", self.__number_of_drawers, " has keyboard tray:",
-              self.__has_keyboard_tray, " max weight capacity:", self.__max_weight_capacity, " current height:",
-              self.__current_height, " max height:", self.__max_height)
-
+    def __str__(self):
+        return f'number of drawers: {self.__number_of_drawers}, has keyboard tray:' \
+               f' {self.__has_keyboard_tray}, max weight capacity: {self.__max_weight_capacity}, current height: ' \
+               f'{self.__current_height}, max height: {self.__max_height}'
 
 writing_desk1 = WritingDesk(1, True, 80, 100, 180)
+print(writing_desk1)
 
 writing_desk2 = WritingDesk(4, False, 70, 100, 180)
+print(writing_desk2)
 
 writing_desk3 = WritingDesk(3, True, 100, 100, 180)
+print(writing_desk3)
 
 writing_desk4 = WritingDesk(2, False, 40, 100, 180)
+print(writing_desk4)
