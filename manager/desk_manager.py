69324fc3c493dd02d16e19d0d7b71a262413c3d4
anastasiaked <stasaked@gmail.com>
2023-05-28 17:23:59 +0300

    this is lab 9

--- manager/desk_manager.py
+++ manager/desk_manager.py
@@ -1,15 +1,67 @@
+from model import desk
+from utils import limiter
 
 
 class DeskManager:
     def __init__(self):
-        self.desks = list()
+        """class constructor"""
+        self.desks = []
+
+    def __len__(self):
+        return len(self.desks)
 
     def add_desk(self, desk):
+        """This method increases the list by adding a new object"""
         self.desks.append(desk)
 
+    def __getitem__(self, index):
+        try:
+            return self.desks[index]
+        except IndexError:
+            return None
+
+    def __iter__(self):
+        """
+        :return:  iterator for insects
+        """
+        return iter(self.desks)
+
     def get_desk(self, index):
+        """this method provides access to individual elements of the "desk" list, which can be useful
+        when performing some operations or obtaining information about a specific desk"""
         return self.desks[index]
 
     def get_desks(self):
+        """this method goes through each element of "self.desks" and with the word yield each element will be returned
+        which causes the method to pause and return a value. The method then saves its state so that the next call
+        continues from the next element in the collection."""
         for desk in self.desks:
             yield desk
+
+    def list_of_results(self):
+        """
+
+        :return: list of heights of desks
+        """
+        return [desk.height for desk in self.desks]
+
+    def enumerate_list(self):
+        """
+
+        :return: ///
+        """
+        return [(desk.__str__(), index) for index, desk in enumerate(self.desks)]
+
+    @limiter(3)
+    def list_of_result_heights(self):
+        """
+
+        :return: ///
+        """
+        result = self.list_of_results()
+        return [(desk,res) for desk, res in zip(self.desks, result)]
+
+    def test_objects(self, test_function):
+        return {"all": all(map(test_function, self.desks)), "any": any(map(test_function, self.desks))}
+
+
--- model/writing_desk.py
+++ model/writing_desk.py
@@ -1,7 +1,9 @@
+"""import Desk"""
 from model.desk import Desk
 
 
 class WritingDesk(Desk):
+    """class"""
     __instance = None
 
     """class constructor """
@@ -16,20 +18,26 @@ class WritingDesk(Desk):
         self.current_height = current_height
         self.max_height = max_height
         super().__init__(height, width, length)
+        self.material_list = {"iron", "wood"}
 
     def __new__(cls, *args, **kwargs):
+        """responsible for checking and creating new instances"""
         if cls.__instance is None:
             cls.__instance = super(WritingDesk, cls).__new__(cls)
         return cls.__instance
 
     def adjust_height(self, centimeters: int):
+        """method that increases the height of the table
+        (if it does not exceed the maximum allowed)"""
         if self.current_height < self.max_height:
             self.current_height += centimeters
 
     def move_down(self, centimeters: int):
+        """a method that reduces the height of the desk"""
         self.current_height -= centimeters
 
-    def set_data(self, number_of_drawers, has_keyboard_tray, max_weight_capacity, current_height, max_height):
+    def set_data(self, number_of_drawers, has_keyboard_tray,
+                 max_weight_capacity, current_height, max_height):
         """method helps to initialize the table object with the specified characteristics"""
         self.__number_of_drawers = number_of_drawers
         self.has_keyboard_tray = has_keyboard_tray
@@ -40,11 +48,7 @@ class WritingDesk(Desk):
     def __str__(self):
         """цей метод виводить обʼєкти по певному шаблону"""
         return f'WritingDesk(number_of_drawers={self.__number_of_drawers}, has_keyboard_tray=' \
-               f' {self.has_keyboard_tray}, max_weight_capacity={self.max_weight_capacity}, current_height=' \
+               f' {self.has_keyboard_tray}, max_weight_capacity={self.max_weight_capacity}, ' \
+               f'current_height=' \
                f'{self.current_height}, max_height={self.max_height})'
 
-    # @staticmethod
-    # def get_instance():
-    #     if WritingDesk.__instance is None:
-    #         WritingDesk.__instance = WritingDesk()
-    #     return WritingDesk.__instance
